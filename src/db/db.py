61e79eb2a1200e18e64963aa563ba38f539d5fb3
Peeter Liik <liikpeeter@gmail.com>
2022-03-30 22:36:50 +0300

    added sent key

--- src/db/db.py
+++ src/db/db.py
@@ -11,7 +11,8 @@ readings = db.readings
 def insert_reading(reading):
     inserted_reading = {
         "value": reading,
-        "time": datetime.datetime.utcnow()
+        "time": datetime.datetime.utcnow(),
+        "sent": False,
     }
     post_id = readings.insert_one(inserted_reading).inserted_id
     return post_id
